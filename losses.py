ca3fc56c87d54d21c98131284a37dee15672bd93
swordswing <eleven@gscw.ru>
2021-10-20 11:45:27 +0200

    fix formatting

--- losses.py
+++ losses.py
@@ -9,26 +9,26 @@ def categorical_focal_loss(alpha=0.25, gamma=2.0):
     if there is skew between different categories/labels in dataset you can try to apply this function as a loss
 
     references
-      https://arxiv.org/pdf/1708.02002.pdf
-      https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
+        https://arxiv.org/pdf/1708.02002.pdf
+        https://www.tensorflow.org/api_docs/python/tf/keras/backend/categorical_crossentropy
 
     example of usage
-      model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25]], gamma=2)], metrics=["accuracy"], optimizer=adam)
+        model.compile(loss=[categorical_focal_loss(alpha=[[.25, .25, .25]], gamma=2)], metrics=["accuracy"], optimizer=adam)
 
     parameters
     ----------
-      alpha : float, default is 0.25
-        the same as weighing factor in balanced cross entropy
-        alpha is used to specify the weight of different categories/labels
-        the size of the array needs to be consistent with the number of classes
+        alpha : float, default is 0.25
+            the same as weighing factor in balanced cross entropy
+            alpha is used to specify the weight of different categories/labels
+            the size of the array needs to be consistent with the number of classes
 
-      gamma : float, default is 2.0
-        focusing parameter for modulating factor
+        gamma : float, default is 2.0
+          focusing parameter for modulating factor
 
     returns
     -------
-      categorical_focal_loss_fixed : function
-        function that that returns mean of focal loss for given batch
+        categorical_focal_loss_fixed : function
+          function that that returns mean of focal loss for given batch
     """
 
     alpha = np.array(alpha, dtype=np.float32)
@@ -39,16 +39,16 @@ def categorical_focal_loss(alpha=0.25, gamma=2.0):
 
         parameters
         ----------
-        y_true : XXX
-           tensor of the same shape as `y_pred`
+            y_true : XXX
+                tensor of the same shape as `y_pred`
 
-        y_pred : XXX
-          tensor resulting from a softmax
+            y_pred : XXX
+              tensor resulting from a softmax
 
         returns
         -------
-        mean_loss : output tensor
-          mean of focal loss
+            mean_loss : output tensor
+              mean of focal loss
         """
 
         # clip the prediction value to prevent NaN's and Inf's
@@ -74,23 +74,23 @@ def binary_focal_loss(alpha=0.25, gamma=2.0):
     binary form of focal loss
 
     references
-      https://arxiv.org/pdf/1708.02002.pdf
+       https://arxiv.org/pdf/1708.02002.pdf
 
     example of usage
-      model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+        model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
 
     parameters
     ----------
-      alpha : float, default is 0.25
-        XXX
+        alpha : float, default is 0.25
+            XXX
 
-      gamma : float, default is 2.0
-        XXX
+        gamma : float, default is 2.0
+            XXX
 
       returns
       -------
         binary_focal_loss_fixed : function
-          function that returns mean of binary loss for given batch
+            function that returns mean of binary loss for given batch
     """
 
     def binary_focal_loss_fixed(y_true, y_pred):
@@ -99,16 +99,16 @@ def binary_focal_loss(alpha=0.25, gamma=2.0):
 
         parameters
         ----------
-          y_true : XXX
-            true labels
+            y_true : XXX
+                true labels
 
-          y_pred : XXX
-            predicted labels
+            y_pred : XXX
+                predicted labels
 
         returns
         -------
-        mean_loss : output tensor
-          mean of focal binary loss
+            mean_loss : output tensor
+                mean of focal binary loss
         """
 
         y_true = tf.cast(y_true, tf.float32)
