e63efe286aed0950cfab0c3bcf05b580a336861c
ztsv-av <ztsv.1337@gmail.com>
2021-11-21 18:41:57 -0700

    new vars

--- globalVariables.py
+++ globalVariables.py
@@ -5,27 +5,44 @@ import albumentations as A
 
 
 # classification
-NUM_EPOCHS = 5
-NUM_CLASSES = 10
-INPUT_SHAPE = (256, 256, 3)
-OUTPUT_ACTIVATION = None  # 'sigmoid', 'softmax', 'relu', None
-FC_LAYERS = (64, None)
+NUM_EPOCHS = 60
+START_EPOCH = 0
+IMAGE_SHAPE = (512, 512, 3)
+NUM_FEATURES = 12
 INITIAL_DROPOUT = 0.5
-DROPOUT_RATES = (0.4, None)
+FC_LAYERS = None
+DROPOUT_RATES = None
 DROP_CONNECT_RATE = 0.4  # 0.2 - default
+OUTPUT_ACTIVATION = 'softmax'  # 'sigmoid', 'softmax', 'relu', None
+NUM_CLASSES = 4
+UNFREEZE = True
+NUM_UNFREEZE_LAYERS = None
+IMAGE_FEATURE_EXTRACTOR_FC = 64
+AUTOENCODER_FC = (256, 64, 256)
 LABELS = ""
 LABEL_TO_INTEGER = ""
 MODEL_POOLING = 'avg'  # 'max', 'avg', None
 
-TRAIN_FILEPATHS = 'projects/testing_animals/data/train/'
-VAL_FILEPATHS = 'projects/testing_animals/data/val/'
-MAX_FILES_PER_PART = 500
+DATA_FILEPATHS = 'projects/petfinder/petfinder-previous/data/images-preprocessed/512/'
+TRAIN_FILEPATHS = 'projects/petfinder/petfinder-previous/data/train/512/'
+VAL_FILEPATHS = 'projects/petfinder/petfinder-previous/data/val/512/'
+DO_KFOLD = True
+NUM_FOLDS = 5
+RANDOM_STATE = 1337
+METADATA = pd.read_csv('projects/petfinder/petfinder-previous/data/metadata/preprocessed_metadata.csv')
+ID_COLUMN = 'id'
+FEATURE_COLUMN = 'Maturity'  # ['Type', 'Age', 'Breed', 'Gender', 'Color', 'Maturity', 'Fur', 'Health']
+FULL_RECORD = False
+MAX_FILES_PER_PART = 300
 MAX_FILEPARTS_TRAIN = len(os.listdir(TRAIN_FILEPATHS)) // MAX_FILES_PER_PART
 MAX_FILEPARTS_VAL = len(os.listdir(VAL_FILEPATHS)) // MAX_FILES_PER_PART
+SHUFFLE_BUFFER_SIZE = 4096
 TRAINED_MODELS_PATH = ''
 TRAINED_MODELS_FILES = None  # os.listdir()
-SAVE_TRAIN_INFO_DIR = 'projects/testing_animals/training/info/'
-SAVE_TRAIN_WEIGHTS_DIR = 'projects/testing_animals/training/weights/'
+SAVE_TRAIN_INFO_DIR = 'projects/petfinder/petfinder-previous/training/info/512/'
+SAVE_TRAIN_WEIGHTS_DIR = 'projects/petfinder/petfinder-previous/training/weights/512/'
+LOAD_WEIGHTS = False
+CLASSIFICATION_CHECKPOINT_PATH = 'projects/petfinder/petfinder-previous/training/weights/512/Xception/fold-4/30/weights.h5'
 
 BATCH_SIZES = {
     'DenseNet121': 16,
@@ -37,14 +54,14 @@ BATCH_SIZES = {
     'EfficientNetB4': 4,
     'EfficientNetB5': 4,
     'InceptionResNetV2': 16,
-    'InceptionV3': 16,
+    'InceptionV3': 8,
     'MobileNet': 16,
     'MobileNetV2': 16,
     'ResNet50': 16,
     'ResNet50V2': 16,
     'ResNet101': 16,
     'ResNet101V2': 16,
-    'Xception': 16}
+    'Xception': 8}
 
 # for 256x256
 BATCH_SIZES_256 = {
@@ -69,7 +86,7 @@ BATCH_SIZES_256 = {
 BATCH_SIZES_512 = {
     'DenseNet121': 16,
     'DenseNet169': 16,
-    'EfficientNetB0': 16,
+    'EfficientNetB0': 8,
     'EfficientNetB1': 16,
     'EfficientNetB2': 16,
     'EfficientNetB3': 16,
@@ -110,11 +127,16 @@ SAVE_TRAIN_INFO_DIR_DETECTION = 'projects/testing_detection/training/csvs/'
 
 # optimizers
 LEARNING_RATE = 0.001
-LR_DECAY_STEPS = 1500
+LR_DECAY_STEPS = 500
 LR_DECAY_RATE = 0.95
+LR_LADDER = True
+LR_LADDER_STEP = 0.75
+LR_LADDER_EPOCHS = 10
+LR_EXP = False
 
 # losses
-LABEL_SMOOTING = 0.01
+FROM_LOGITS = False
+LABEL_SMOOTING = 0.001
 
 # metrics
 # use 0.0 when loss = BinaryCrossentropy(from_logits=True), otherwise 0.5 or any desired value
@@ -135,7 +157,7 @@ PNG_SAVE_PATH = ''
 # melspectogram finetune (birdclef 2020-21)
 SAMPLING_RATE = 21952
 SIGNAL_LENGTH = 5  # seconds
-HOP_LENGTH = int(SIGNAL_LENGTH * SAMPLING_RATE / (INPUT_SHAPE[1] - 1))
+HOP_LENGTH = int(SIGNAL_LENGTH * SAMPLING_RATE / (IMAGE_SHAPE[1] - 1))
 N_FFT = 1536
 FMIN = 500
 FMAX = 12500
@@ -148,7 +170,7 @@ DOWNSCALE_MIN = 0.1
 DOWNSCALE_MAX = 0.3
 GAUSSIAN_BLUR_LIMIT = (5, 9)
 GLASS_BLUR_ITERATIONS = 1
-GLASS_BLUR_MAXDELTA = 2
+GLASS_BLUR_MAXDELTA = 3
 GAMMA_LIMIT = (96, 164)
 EMBOSS_STRENGTH = (0.4, 1)
 SHARPEN_ALPHA = (0.2, 0.7)
@@ -158,8 +180,8 @@ OPTICAL_SHIFT_LIMIT = 0.5  # doesn't really do much
 ROTATE_LIMIT = 30
 INVERT_PROBABILITY = 0.5
 
-DO_PERMUTATIONS = False
-PERMUTATION_PROBABILITY_CLASSIFICATION = 1 / 5
+DO_PERMUTATIONS = True
+PERMUTATION_PROBABILITY_CLASSIFICATION = 1 / 4
 PERMUTATIONS_CLASSIFICATION = [
     A.RandomGamma(gamma_limit=GAMMA_LIMIT,
                   p=PERMUTATION_PROBABILITY_CLASSIFICATION),
