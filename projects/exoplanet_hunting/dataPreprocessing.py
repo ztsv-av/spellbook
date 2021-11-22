beb4b6722799b6bf841f1012f652a194d7fc2466
ztsv-av <ztsv.1337@gmail.com>
2021-11-21 19:28:05 -0700

    fix functions

--- projects/exoplanet_hunting/dataPreprocessing.py
+++ projects/exoplanet_hunting/dataPreprocessing.py
@@ -6,16 +6,48 @@ from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import Pipeline
 
 
-exoTrain = pd.read_csv('datasets/exoTrain.csv')
-exoTest = pd.read_csv('datasets/exoTest.csv')
+TRAIN_META = pd.read_csv('datasets/TRAIN_META.csv')
+TEST_META = pd.read_csv('datasets/TEST_META.csv')
+
+
+# Change classes from 1, 2 to 0, 1
+def lableChange(y_train, y_test):
+    labler = lambda x: 1 if x == 2 else 0
+    y_train_01, y_test_01 = y_train.apply(labler), y_test.apply(labler)
+
+    return y_train_01, y_test_01
+
+
+# Define training and testing datasets
+def datasets():
+
+    x_train, y_train = TRAIN_META.loc[:, TRAIN_META.columns != 'LABEL'], TRAIN_META.loc[:, 'LABEL']
+    x_test, y_test = TEST_META.loc[:, TEST_META.columns != 'LABEL'], TEST_META.loc[:, 'LABEL']
+
+    #fill NaNs with mean (no NaNs)
+    #for column in x_train:
+        #x_train[column] = x_train[column].fillna(x_train[column].mean(), 2))
+
+    #x_train, x_test = scale(x_train, x_test)
+    #x_train = handle_outliers(x_train, 1)
+    #print(sum(y_train > 1), sum(y_train < 2))
+    x_train, y_train = smote(x_train, y_train)
+    #print(sum(y_train > 1), sum(y_train < 2))
+    y_train, y_test = lableChange(y_train, y_test)
+
+    n_features = x_train.shape[1]
+
+    return x_train, y_train, x_test, y_test, n_features
+
 
 # Change value of outliers to mean
-def handle_outliers(dataset, num_iterations):
-    
+def handleOutliers(dataset, num_iterations):
+
     dataset_handled = dataset
     threshold = 100
 
     for n in range(num_iterations):
+
         # for column in range(dataset_handled.shape[0]):
         # for index, row in dataset_handled.iterrows():
             # row_values = row.values
@@ -45,14 +77,8 @@ def handle_outliers(dataset, num_iterations):
                 elif abs(val) > threshold * abs(next_val):
                     dataset_handled.iloc[index][i] = dataset_handled.iloc[index][i + 1]
 
-    return dataset_handled 
-            
-# Change classes from 1, 2 to 0, 1
-def lable_change(y_train, y_test):
-    labler = lambda x: 1 if x == 2 else 0
-    y_train_01, y_test_01 = y_train.apply(labler), y_test.apply(labler)
+    return dataset_handled
 
-    return y_train_01, y_test_01
 
 # Scale data
 def scale(x_train, x_test):
@@ -62,8 +88,10 @@ def scale(x_train, x_test):
 
     return x_train_scaled, x_test_scaled
 
+
 # SMOTE
 def smote(x_train, y_train):
+
     #smote = SMOTE(random_state=17, sampling_strategy='minority')
     over = SMOTE(sampling_strategy=0.2)
     under = RandomUnderSampler(sampling_strategy=0.3)
@@ -73,28 +101,10 @@ def smote(x_train, y_train):
 
     return x_train_res, y_train_res
 
-# Define training and testing datasets
-def datasets():
-    x_train, y_train = exoTrain.loc[:, exoTrain.columns != 'LABEL'], exoTrain.loc[:, 'LABEL']
-    x_test, y_test = exoTest.loc[:, exoTest.columns != 'LABEL'], exoTest.loc[:, 'LABEL']
-    
-    #fill NaNs with mean (no NaNs)
-    #for column in x_train:
-        #x_train[column] = x_train[column].fillna(x_train[column].mean(), 2))
-
-    #x_train, x_test = scale(x_train, x_test)
-    #x_train = handle_outliers(x_train, 1)
-    #print(sum(y_train > 1), sum(y_train < 2))
-    x_train, y_train = smote(x_train, y_train)
-    #print(sum(y_train > 1), sum(y_train < 2))
-    y_train, y_test = lable_change(y_train, y_test)
-
-    n_features = x_train.shape[1]
-
-    return x_train, y_train, x_test, y_test, n_features
 
 # Graph of data
-def flux_graph(dataset, row, planet):
+def fluxGraph(dataset, row, planet):
+
     fig = plt.figure(figsize=(20,5), facecolor=(.18, .31, .31))
     ax = fig.add_subplot()
     ax.set_facecolor('#004d4d')
@@ -108,15 +118,17 @@ def flux_graph(dataset, row, planet):
     ax.tick_params(colors = 'black', labelcolor='#00ffff', labelsize=14)
     plt.show()
 
+
 # Show graph of data
-def show_graph():
-    with_planet = exoTrain[exoTrain['LABEL'] == 2].head(2).index
-    wo_planet = exoTrain[exoTrain['LABEL'] == 1].head(2).index
-    
+def showGraph():
+
+    with_planet = TRAIN_META[TRAIN_META['LABEL'] == 2].head(2).index
+    wo_planet = TRAIN_META[TRAIN_META['LABEL'] == 1].head(2).index
+
     dataset, _, _, _, _ = datasets()
     time_points = list(dataset.columns)
 
     for row in with_planet:
-        flux_graph(dataset, row, planet = 'with_planet')
+        fluxGraph(dataset, row, planet = 'with_planet')
     for row in wo_planet:
-        flux_graph(dataset, row, planet = 'wo_planet')
+        fluxGraph(dataset, row, planet = 'wo_planet')
