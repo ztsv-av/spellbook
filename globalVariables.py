9a0240238281a173c9899b0f9abbe5d75033e1c8
ztsv-av <ztsv.1337@gmail.com>
2022-01-24 23:09:51 -0700

    add new vars and format fix

--- globalVariables.py
+++ globalVariables.py
@@ -1,54 +1,11 @@
 import numpy as np
 import pandas as pd
-import os
 import albumentations as A
 
 
 # classification
 NUM_EPOCHS = 60
 START_EPOCH = 0
-INPUT_SHAPE = (256, 256, 3)
-CONCAT_FEATURES_BEFORE = True
-CONCAT_FEATURES_AFTER = False
-NUM_FEATURES = [2, 167, 3, 4, 7, 3] # [2, 3, 7, 167, 3, 4]
-INITIAL_DROPOUT = 0.2
-FC_LAYERS = (512, 64)
-DROPOUT_RATES = (0.2, None)
-DROP_CONNECT_RATE = 0.4  # 0.2 - default
-OUTPUT_ACTIVATION = None  # 'sigmoid', 'softmax', 'relu', None
-NUM_CLASSES = 1
-DO_BATCH_NORM = False
-DO_PREDICTIONS = True
-UNFREEZE = True
-UNFREEZE_FULL = False
-NUM_UNFREEZE_LAYERS = 63
-IMAGE_FEATURE_EXTRACTOR_FC = 64
-AUTOENCODER_FC = (256, 64, 256)
-LABELS = ""
-LABEL_TO_INTEGER = ""
-MODEL_POOLING = 'avg'  # 'max', 'avg', None
-DATA_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed/'
-ADDITIONAL_DATA_FILEPATHS = ''
-TRAIN_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed-train/'
-VAL_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed-val/'
-DO_VALIDATION = True
-DO_KFOLD = False
-NUM_FOLDS = 5
-RANDOM_STATE = 1337
-METADATA = pd.read_csv('projects/petfinder/petfinder-previous/data/metadata/age-full-last-predicted-breeds.csv')
-ID_COLUMN = 'id'
-FEATURE_COLUMN = 'popularity'
-FULL_RECORD = False
-MAX_FILES_PER_PART = 900
-SHUFFLE_BUFFER_SIZE = 4096
-TRAINED_MODELS_PATH = ''
-TRAINED_MODELS_FILES = None  # os.listdir()
-SAVE_TRAIN_INFO_DIR = 'projects/petfinder/petfinder-previous/training/info/'
-SAVE_TRAIN_WEIGHTS_DIR = 'projects/petfinder/petfinder-previous/training/weights/'
-LOAD_WEIGHTS = False
-LOAD_MODEL = False
-CLASSIFICATION_CHECKPOINT_PATH = 'projects/petfinder/petfinder-previous/training/weights/InceptionV3/no-folds/8/savedModel/'
-
 BATCH_SIZES = {
     'VGG16': 16,
     'VGG19': 16,
@@ -69,59 +26,71 @@ BATCH_SIZES = {
     'ResNet101': 16,
     'ResNet101V2': 16,
     'Xception': 16}
+    
+INPUT_SHAPE = (256, 256, 3)
 
-# for 256x256
-BATCH_SIZES_256 = {
-    'DenseNet121': 128,
-    'EfficientNetB0': 16,
-    'EfficientNetB1': 16,
-    'EfficientNetB2': 8,
-    'EfficientNetB3': 8,
-    'EfficientNetB4': 4,
-    'EfficientNetB5': 1,
-    'InceptionResNetV2': 8,
-    'InceptionV3': 16,
-    'MobileNet': 16,
-    'MobileNetV2': 16,
-    'ResNet50': 16,
-    'ResNet50V2': 16,
-    'ResNet101': 8,
-    'ResNet101V2': 8,
-    'Xception': 4}
+LOAD_FEATURES = False
+NUM_FEATURES = [2, 167, 3, 4, 7, 3] # [2, 3, 7, 167, 3, 4]
+CONCAT_FEATURES_BEFORE = True
+CONCAT_FEATURES_AFTER = False
 
-# for 512x512 (some over memory)
-BATCH_SIZES_512 = {
-    'DenseNet121': 16,
-    'DenseNet169': 16,
-    'EfficientNetB0': 8,
-    'EfficientNetB1': 16,
-    'EfficientNetB2': 16,
-    'EfficientNetB3': 16,
-    'EfficientNetB4': 16,
-    'EfficientNetB5': 16,
-    'InceptionResNetV2': 16,
-    'InceptionV3': 16,
-    'MobileNet': 16,
-    'MobileNetV2': 16,
-    'ResNet50': 16,
-    'ResNet50V2': 16,
-    'ResNet101': 16,
-    'ResNet101V2': 16,
-    'Xception': 16}
+MODEL_POOLING = 'avg'  # 'max', 'avg', None
+DROP_CONNECT_RATE = 0.2  # 0.2 - default
+INITIAL_DROPOUT = 0.2
+DO_BATCH_NORM = False
+FC_LAYERS = (512, 64)
+DROPOUT_RATES = (0.2, None)
+
+DO_PREDICTIONS = True
+OUTPUT_ACTIVATION = None  # 'sigmoid', 'softmax', 'relu', None
+NUM_CLASSES = 1
+
+UNFREEZE = True
+UNFREEZE_FULL = False
+NUM_UNFREEZE_LAYERS = 63
+
+LOAD_WEIGHTS = False
+LOAD_MODEL = False
+
+DATA_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed/'
+TRAIN_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed-train/'
+VAL_FILEPATHS = 'projects/petfinder/petfinder-previous/data/petfinder-images-preprocessed-val/'
+DO_VALIDATION = True
+MAX_FILES_PER_PART = 900
+SHUFFLE_BUFFER_SIZE = 4096
+RANDOM_STATE = 1337
+
+METADATA = pd.read_csv('projects/petfinder/petfinder-previous/data/metadata/age-full-last-predicted-breeds.csv')
+ID_COLUMN = 'id'
+FEATURE_COLUMN = 'popularity'
+FULL_RECORD = False
+
+DO_KFOLD = False
+NUM_FOLDS = 5
+
+TRAINED_MODELS_PATH = ''
+CLASSIFICATION_CHECKPOINT_PATH = 'projects/petfinder/petfinder-previous/training/weights/InceptionV3/no-folds/8/savedModel/'
+
+SAVE_TRAIN_INFO_DIR = 'projects/petfinder/petfinder-previous/training/info/'
+SAVE_TRAIN_WEIGHTS_DIR = 'projects/petfinder/petfinder-previous/training/weights/'
 
 # object detection
 MODEL_NAME_DETECTION = 'effdet0'
+
 NUM_EPOCHS_DETECTION = 2
 BATCH_SIZE_DETECTION = 2
+
 INPUT_SHAPE_DETECTION = (512, 512)
 DUMMY_SHAPE_DETECTION = (1, 512, 512, 3)
+
 NUM_CLASSES_DETECTION = 1
+
 LABEL_ID_OFFSET = 1
+
 IMAGE_TYPE = np.uint8
 BBOX_FORMAT = 'albumentations'
 
-# change only /checkpoint_.../
-CHECKPOINT_PATH = 'object_detection/models/research/object_detection/test_data/checkpoint_efficientdet_d0/ckpt-0'
+CHECKPOINT_PATH = 'object_detection/models/research/object_detection/test_data/checkpoint_efficientdet_d0/ckpt-0'  # change only /checkpoint_.../
 CONFIG_PATH = 'object_detection/models/research/object_detection/configs/tf2/ssd_efficientdet_d0_512x512_coco17_tpu-8.config'
 TRAIN_FILEPATHS_DETECTION = 'projects/testing_detection/datasets/train/'
 TRAIN_META_DETECTION = pd.read_csv(
@@ -134,9 +103,11 @@ SAVE_TRAIN_INFO_DIR_DETECTION = 'projects/testing_detection/training/csvs/'
 
 # optimizers
 LEARNING_RATE = 0.001
+
 LR_EXP = False
 LR_DECAY_STEPS = 500
 LR_DECAY_RATE = 0.95
+
 LR_LADDER = True
 LR_LADDER_STEP = 0.5
 LR_LADDER_EPOCHS = 15
@@ -147,8 +118,7 @@ LABEL_SMOOTHING = 0.005
 LOSS_REDUCTION = None
 
 # metrics
-# use 0.0 when loss = BinaryCrossentropy(from_logits=True), otherwise 0.5 or any desired value
-ACCURACY_THRESHOLD = 0.0
+ACCURACY_THRESHOLD = 0.0  # use 0.0 when loss = BinaryCrossentropy(from_logits=True), otherwise 0.5 or any desired value
 F1_SCORE_AVERAGE = 'macro'
 
 # callbacks
@@ -161,6 +131,13 @@ OVERFITTING_THRESHOLD = 1.3
 NUM_CHANNELS = 3
 NUMPY_SAVE_PATH = ''
 PNG_SAVE_PATH = ''
+
+# denoising autoencoder
+BUILD_AUTOENCODER = False
+DENSE_NEURONS_DATA_FEATURES = 64
+DENSE_NEURONS_ENCODER = 256
+DENSE_NEURONS_BOTTLE = 64
+DENSE_NEURONS_DECODER = 256
 
 # melspectogram finetune (birdclef 2020-21)
 SAMPLING_RATE = 21952
